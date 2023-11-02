71f278c6b908c1b505dbb712bb2d1166a35cf4be
DueViktor <viktorduep@gmail.com>
2023-11-02 11:26:29 +0100

    bug fix

--- scripts/4-build-matrix-for-nvd.py
+++ scripts/4-build-matrix-for-nvd.py
@@ -3,7 +3,8 @@ Description:
     The script to build the matrix for node vector distance (NVD) calculations.
     The matrix is a matrix where each column is a politician and each row is a donor.
     The cell value is the amount of donation from the donor to the politician, log-transformed, 
-    and normalized by the total amount of donation from the donor (i.e., the sum of all cells in the row).
+    
+    Perhaps TODO: and normalized by the total amount of donation from the donor (i.e., the sum of all cells in the row).
 
     The actual calculation is done in Julia, see scripts/5-nvd.jl.
 
@@ -16,10 +17,8 @@ from pathlib import Path
 
 import networkx as nx
 import numpy as np
-import pandas as pd
 from tqdm import tqdm
-
-from scripts.utils import load_json, save_json
+from utils import read_json, save_json
 
 
 def generate_custom_id(G_largest: nx.Graph) -> None:
@@ -29,9 +28,9 @@ def generate_custom_id(G_largest: nx.Graph) -> None:
     littlesis_id_to_my_id = {}
     my_id_to_littlesis_id = {}
 
-    for i, node in enumerate(G_largest.nodes()):
-        littlesis_id_to_my_id[G_largest.nodes[node]["id"]] = i
-        my_id_to_littlesis_id[i] = G_largest.nodes[node]["id"]
+    for i, node_id in enumerate(G_largest.nodes()):
+        my_id_to_littlesis_id[i + 1] = node_id
+        littlesis_id_to_my_id[node_id] = i + 1
 
     # Save the mappings to JSON files
     save_json(
@@ -47,13 +46,17 @@ def generate_network_csv(G_largest: nx.Graph) -> None:
     Generate network.csv, which contains the edges of the graph in the format "a,b."
     Note that the IDs are custom-created.
     """
-    littlesis_id_to_my_id = load_json(Path("data/id-mapping-littlesis-to-mine.json"))
+    littlesis_id_to_my_id = read_json(
+        path=Path("data/id-mapping-littlesis-to-mine.json")
+    )
 
-    with open("../data/nvd/network.csv", "w") as f:
-        for edge in tqdm(G_largest.edges()):
+    with open("data/nvd-network.csv", "w") as f:
+        for edge in tqdm(G_largest.edges(), desc="Writing network.csv"):
             littlesis_src_id, littlesis_dst_id = edge
-            src_id = littlesis_id_to_my_id[littlesis_src_id]
-            dst_id = littlesis_id_to_my_id[littlesis_dst_id]
+
+            src_id = littlesis_id_to_my_id[str(littlesis_src_id)]
+            dst_id = littlesis_id_to_my_id[str(littlesis_dst_id)]
+
             f.write(f"{src_id},{dst_id}\n")
 
 
@@ -61,37 +64,35 @@ def generate_node_attributes_csv(
     donation_to_politician: dict, my_id_to_littlesis_id: dict
 ) -> None:
     # Generate node_attributes.csv from donation_to_politician
-    with open("../data/nvd/node_attributes.csv", "w") as f:
+    with open("data/nvd-node_attributes.csv", "w") as f:
         f.write("donor,")
         politician_ids = [
             str(politician_id) for politician_id in set(donation_to_politician.keys())
         ]
         f.write(",".join(politician_ids) + "\n")
 
-        for my_donor_id, littlesis_donor_id in tqdm(my_id_to_littlesis_id.items()):
+        for my_donor_id, littlesis_donor_id in tqdm(
+            my_id_to_littlesis_id.items(), desc="Writing nvd-node_attributes.csv"
+        ):
             line = f"{my_donor_id},"
+            littlesis_donor_id = str(littlesis_donor_id)
             for politician in politician_ids:
                 if littlesis_donor_id in donation_to_politician[politician]:
                     donation = donation_to_politician[politician][littlesis_donor_id]
-                    line += f"{donation},"
+                    if donation < 0:  # Negative donations are not allowed
+                        donation = 0
+                    logged_donation = np.log(donation + 1)
+                    line += f"{logged_donation},"
                 else:
-                    line += "0,"
+                    line += "0,"  # np.log(1) = 0
             f.write(line[:-1] + "\n")
-
-    # Log-transform and normalize
-    df = pd.read_csv("../data/nvd/node_attributes.csv", index_col=0)
-
-    df = df.apply(lambda x: x + 1)
-    df = df.apply(lambda x: x.apply(lambda y: np.log(y)))
-    df = df.apply(lambda x: x / x.sum(), axis=1)
-    df.to_csv("../data/nvd/node_attributes.csv")
 
 
 if __name__ == "__main__":
     import pickle
 
     # Load the largest component in the Littlesis network
-    G_largest = pickle.load(open("../data/littlesis-largest-component.pickle", "rb"))
+    G_largest = pickle.load(open("data/littlesis-largest-component.pickle", "rb"))
 
     # Generate custom IDs
     generate_custom_id(G_largest)
@@ -100,10 +101,10 @@ if __name__ == "__main__":
     generate_network_csv(G_largest)
 
     # Load donation data
-    donation_to_politician = load_json(Path("data/donation-to-politician.json"))
+    donation_to_politician = read_json(Path("data/donation_to_politician.json"))
 
     # Generate node attributes
     generate_node_attributes_csv(
         donation_to_politician,
-        load_json(Path("data/id-mapping-mine-to-littlesis.json")),
+        read_json(Path("data/id-mapping-mine-to-littlesis.json")),
     )
